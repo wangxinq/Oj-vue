d00b4f8dae7af4228276c0e09d1e52a820ae1f92
wangxinq <2154659447@qq.com>
2025-02-23 09:48:38 +0800

    前端初始化

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="GlobalHeader">
+    <div class="menu-demo">
+      <a-menu mode="horizontal" :default-selected-keys="['1']">
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/oj-logo.jpg" />
+            <div class="title">WOJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item key="1">Home</a-menu-item>
+        <a-menu-item key="2">Solution</a-menu-item>
+        <a-menu-item key="3">Cloud Service</a-menu-item>
+        <a-menu-item key="4">Cooperation</a-menu-item>
+      </a-menu>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+import { routes } from "@/router/routes";
+<style scoped>
+.title-bar {
+  display: flex;
+}
+
+.logo {
+  height: 48px;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+  align-content: center;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="www.wz.com" target="_blank">华中农业大学 by 王新强</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #eee, #fff);
+}
+
+#basicLayout .footer {
+  background: #efefef;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
